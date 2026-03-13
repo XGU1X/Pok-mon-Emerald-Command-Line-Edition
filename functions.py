4650b1ae3de66fe654661fa826d0b23a1c97239a
XGU1X <3971360827@qq.com>
2026-03-13 21:18:06 +0800

    Update functions.py
    重构了main_interface函数

--- functions.py
+++ functions.py
@@ -1,27 +1,8 @@
 import glob,keyboard,os,time,sys
-def main_interface(interface_type,num):
-    txt_files = glob.glob('*.txt')
-    print("\n精灵宝可梦\n        绿宝石\n\n",end="")
-    if interface_type== True:
-        print("新存档     ")  
-        for i in range(len(txt_files)):
-            print("存档"+txt_files[i].split(".")[0]+"     ")
-    elif interface_type== False:
-        for i in range(len(txt_files)+1):
-            if i== 0:  
-                if num== 0:
-                    print(">新存档     ")
-                else:
-                    print("新存档     ")
-            else:  
-                if num== i:
-                    print(">存档"+txt_files[i-1].split(".")[0]+"     ")
-                else:
-                    print("存档"+txt_files[i-1].split(".")[0]+"     ")
 def dialog_box(options_list,text):
     max_choice_num = 0
     i = 0
-    choice_num = 0
+    choice_num = -1
     max_choice_num = len(options_list) - 1
     if max_choice_num+1== 0:
         os.system("cls")
@@ -57,7 +38,6 @@ def dialog_box(options_list,text):
                 choice_num += 1
                 if choice_num > max_choice_num:
                     choice_num = 0
-                
                 for j in range(max_choice_num + 1):
                     if j == choice_num:
                         print(">" + options_list[j])
@@ -65,8 +45,17 @@ def dialog_box(options_list,text):
                         print(options_list[j])
                 time.sleep(0.15)
             elif keyboard.is_pressed("enter"):
-                return choice_num
+                if choice_num!= -1:
+                    return choice_num
             time.sleep(0.05)
+def main_interface():
+    txt_files = glob.glob('*.txt')
+    dialog_box_list = []
+    dialog_box_list.append("新存档")
+    for i in range(len(txt_files)):
+        dialog_box_list.append("存档"+txt_files[i].split(".")[0])
+    archive = dialog_box(dialog_box_list,"\n精灵宝可梦\n        绿宝石\n")
+    return archive
 def location_cutscenes(text):
     os.system("cls")
     for i in range(os.get_terminal_size().lines):
@@ -77,3 +66,4 @@ def location_cutscenes(text):
         print(" "*os.get_terminal_size().columns)
     time.sleep(0.5)
     os.system("cls")
+    
